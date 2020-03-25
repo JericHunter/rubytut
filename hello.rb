fbbfe50d3b86e8000643fd2f297e43a5d8b7b1d6
JericHunter <jeric.hunterr@gmail.com>
2020-03-25 15:11:51 -0400

    v4

--- hello.rb
+++ hello.rb
@@ -9,7 +9,7 @@ puts my_boolean
 puts my_string
 
 puts "What's up?"
-print "Oxnard Montalvo"
+print "Nothing much"
 
 puts "Jeric".length
 puts "Jeric".reverse
@@ -21,3 +21,14 @@ puts "Jeric".downcase
 =begin
 everything in between begin and end is a comment
 =end
+# make sure to have variable names start with lowercase as using uppercase can cause issues because they may have different meanings
+name = "Jeric"
+my_age = 19
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+puts sum
+puts product
+puts quotient
