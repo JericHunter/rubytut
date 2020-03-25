7df928aee982d8c3a16bfedbacd4dbb0a0fd275a
JericHunter <jeric.hunterr@gmail.com>
2020-03-25 15:21:16 -0400

    v6

---
+++ format.rb
@@ -0,0 +1,6 @@
+print "What's your first name? "
+first_name = gets.chomp # "get" gets the input from the user and chomp gets rid of the extra line
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
